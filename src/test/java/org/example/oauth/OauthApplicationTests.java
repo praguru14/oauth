0de999e207ff81b77c8d4b63cad3a2736803301a
Prafull Gururani <praguru14@gmail.com>
2026-05-23 10:37:03 +0530

    oidcUser

--- src/test/java/org/example/oauth/OauthApplicationTests.java
+++ src/test/java/org/example/oauth/OauthApplicationTests.java
@@ -1,9 +1,17 @@
 package org.example.oauth;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {
+        "spring.security.oauth2.client.registration.azure.client-id=test-client",
+        "spring.security.oauth2.client.registration.azure.client-secret=test-secret",
+        "spring.security.oauth2.client.provider.azure.issuer-uri=http://localhost:9000"
+})
 class OauthApplicationTests {
 
     @Test
