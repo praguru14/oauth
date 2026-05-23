1a1b0a490dd2fce7db113a2741795f2baed6a68f
Prafull Gururani <praguru14@gmail.com>
2026-05-23 10:31:29 +0530

    oidcUser

--- src/main/java/org/example/oauth/OauthTokenService.java
+++ src/main/java/org/example/oauth/OauthTokenService.java
@@ -1,4 +1,23 @@
 package org.example.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OauthTokenService {
+
+    @Autowired
+    private OAuth2AuthorizedClientService auth2AuthorizedClientService;
+
+    public String getAccessToen(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticationToken oauth = (OAuth2AuthenticationToken) auth;
+        OAuth2AuthorizedClient client = auth2AuthorizedClientService.loadAuthorizedClient(oauth.getAuthorizedClientRegistrationId(), oauth.getName());
+    return client.getAccessToken().getTokenValue();
+    }
 }
--- src/main/java/org/example/oauth/TestController.java
+++ src/main/java/org/example/oauth/TestController.java
@@ -1,20 +1,29 @@
 package org.example.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
+    
+    @Autowired
+    OauthTokenService oauthTokenService;
 
     // Public page
     @GetMapping("/")
     public String home() {
 
+        
         return """
                 <html>
 
@@ -32,72 +41,97 @@ public class TestController {
                 """;
     }
 
-    // Protected page
     @GetMapping("/dashboard")
-    public String dashboard(
+    public String dashboard() {
 
-    ) {
-final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object username =
-                authentication.getPrincipal();
+        final Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
 
-        OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+        OidcUser oidcUser =
+                (OidcUser) authentication.getPrincipal();
 
-        String user = oidcUser.getAttribute("preferred_username");
+        String user =
+                oidcUser.getAttribute("preferred_username");
+
+        String accessToken =
+                oauthTokenService.getAccessToen();
 
         return """
-                <html>
+            <html>
 
-                <body>
+            <body>
 
-                    <h1>You are authenticated</h1>
+                <h1>You are authenticated</h1>
 
-                    <h2>Welcome %s</h2>
+                <h2>Welcome %s</h2>
 
-                    <br>
+                <br>
 
-                    <button onclick="loadApi('/api1')">
-                        Load API 1
-                    </button>
+                <button onclick="loadApi('/api1')">
+                    Load API 1
+                </button>
 
-                    <button onclick="loadApi('/api2')">
-                        Load API 2
-                    </button>
+                <button onclick="loadApi('/api2')">
+                    Load API 2
+                </button>
 
-                    <button onclick="loadApi('/api3')">
-                        Load API 3
-                    </button>
+                <button onclick="loadApi('/api3')">
+                    Load API 3
+                </button>
 
-                    <br><br>
+                <br><br>
 
-                    <pre id='result'></pre>
+                <pre id='result'></pre>
 
-                    <script>
+                <script>
 
-                        async function loadApi(url) {
+                    const accessToken = '%s';
 
-                            const response =
-                                await fetch(url);
+                    async function loadApi(url) {
 
-                            const text =
-                                await response.text();
+                        const response = await fetch(url, {
 
-                            document.getElementById('result')
-                                .innerText = text;
-                        }
+                            method: 'GET',
 
-                    </script>
+                            headers: {
+                                'Authorization':
+                                    'Bearer ' + accessToken
+                            }
 
-                </body>
+                        });
 
-                </html>
-                """.formatted(user);
+                        const text =
+                            await response.text();
+
+                        document.getElementById('result')
+                            .innerText = text;
+                    }
+
+                </script>
+
+            </body>
+
+            </html>
+            """.formatted(user, accessToken);
     }
 
     @GetMapping("/api1")
-    public String api1() {
+    public String api1(Authentication authentication) {
 
-        return "Protected API 1 called successfully";
+        JwtAuthenticationToken token =
+                (JwtAuthenticationToken) authentication;
+
+        Jwt jwt = token.getToken();
+
+        List<String> roles =
+                jwt.getClaimAsStringList("groups");
+
+        if (roles != null && roles.contains("ADMIN")) {
+
+            return "Welcome ADMIN";
+        }
+
+        return "Access Denied";
     }
 
     @GetMapping("/api2")
